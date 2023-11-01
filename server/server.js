87a67156d876e726eb242d2c6bb7d8e741930e25
n0wlk <kxwxn@icloud.com>
2023-11-01 22:55:41 +0900

    tiny adjustments

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const tokyoData = require("./data/datas.json");
 
 app.use(cors());
 
+app.use("/assets", express.static("assets"));
+
+// app.use('/static', express.static(path.join(__dirname, 'public')))  --- 절대경로 , 그런데 작동하지 않고 오류남.
+
 app.get("/api/tokyo", (req, res) => {
   res.json(tokyoData);
 });
