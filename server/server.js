c357c1e876cedd47874f8d89de32b8956d1f0945
n0wlk <kxwxn@icloud.com>
2023-11-02 16:08:09 +0900

    url edited

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3003;
 const cors = require("cors");
-const tokyoData = require("./data/datas.json");
+const tokyoData = require("./data/tokyo.json");
+const path = require("path");
 
 app.use(cors());
 
-app.use("/assets", express.static("assets"));
+// app.use("/assets", express.static("assets"));
 
-// app.use('/static', express.static(path.join(__dirname, 'public')))  --- 절대경로 , 그런데 작동하지 않고 오류남.
+const assetsPath = path.join(__dirname, "assets");
+app.use("/assets", express.static(assetsPath));
 
 app.get("/api/tokyo", (req, res) => {
   res.json(tokyoData);
